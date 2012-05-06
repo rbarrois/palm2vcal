3d39ff048047bfe5e59376a8607c2b6476fc0954
Raphaël Barrois <raphael.barrois@polytechnique.org>
2012-05-07 01:14:51 +0200

    Add setup.py
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2011-2012 Raphaël Barrois
+
+from distutils.core import setup
+from distutils import cmd
+import os
+import re
+
+def get_version():
+    version_re = re.compile(r"^__version__ = '([\w_.]+)'$")
+    with open(os.path.join(os.path.dirname(__file__), 'palm2vcal', '__init__.py')) as f:
+        for line in f:
+            match = version_re.match(line[:-1])
+            if match:
+                return match.groups()[0]
+    return '0.0'
+
+
+setup(
+    name='palm2vcal',
+    version=get_version(),
+    description="Convert PalmOS .dba calendar to a vcalendar file.",
+    author='Raphaël Barrois',
+    author_email='raphael.barrois@polytechnique.org',
+    url='http://github.com/rbarrois/palm2vcal',
+    download_url="http://pypi.python.org/pypi/palm2vcal/",
+    keywords=['palm', 'calendar', 'conversion', 'vcalendar', 'ics'],
+    packages=['palm2vcal'],
+    scripts=['bin/palm2vcal'],
+    license='MIT',
+    requires=[
+        'icalendar',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Office/Business :: Scheduling',
+    ],
+)
+
+
